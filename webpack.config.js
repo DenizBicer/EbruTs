590466dce0f56415042ea8f7d6c8b9a6c6ea31f8
Deniz Bicer <contact@denizbicer.com>
2022-11-11 14:17:11 +0100

    reconfigured webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -20,5 +20,13 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'docs')
-    }
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'docs'),
+        },
+        compress: true,
+        port: 9000,
+    },
+
 }

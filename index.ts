8c36b0ec4ed31434bd926d29f401c15642699e5b
Deniz Bicer <contact@denizbicer.com>
2022-11-16 23:13:47 +0100

    added a main page

---
+++ index.ts
@@ -0,0 +1,12 @@
+
+import './src/styles/reset.css'
+import './src/styles/index.css'
+import './src/styles/article-image.css'
+import './src/styles/article.css'
+import './src/styles/button.css'
+import './src/styles/text.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<a href="./dist/src/pages/inkdrop/">Ink Drop</a>
+
+`
